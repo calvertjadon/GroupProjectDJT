26307ff298d509f24a3b1ce026bcd5e94d8b01d5
Dontae Moore <dontae_moore132@mymail.eku.edu>
2020-11-20 18:56:31 -0500

    showevents

--- ShowEvents.cs
+++ ShowEvents.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MySql.Data.MySqlClient;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -15,6 +16,77 @@ namespace GroupProjectDJT
         public ShowEvents()
         {
             InitializeComponent();
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void ShowEvents_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            dataGridView1.Rows.Clear();
+
+            string connStr = "server=157.89.28.130;user=ChangK;database=csc340;port=3306;password=Wallace#409;";
+            MySql.Data.MySqlClient.MySqlConnection conn3 = new MySql.Data.MySqlClient.MySqlConnection(connStr);
+
+            String eventID;
+            String name;
+            String description;
+            String time;
+            String price;
+            String vipPrice;
+            String date;
+
+
+
+            try
+            {
+                
+                Console.WriteLine("Connecting to MySQL...");
+                conn3.Open();
+                string sql = "SELECT eventID, name, description, date,time, price, vipPrice FROM djt_event";
+
+
+                MySql.Data.MySqlClient.MySqlCommand cmd = new MySql.Data.MySqlClient.MySqlCommand(sql, conn3);
+
+
+
+
+                MySqlDataReader myReader = cmd.ExecuteReader();
+                while (myReader.Read())
+                {
+
+                    eventID = myReader["eventID"].ToString();
+                    name = myReader["name"].ToString();
+                    description = myReader["description"].ToString();
+                    date = myReader["date"].ToString() + "$";
+                    time = myReader["time"].ToString();
+                    price = myReader["price"].ToString();
+                    vipPrice = myReader["vipPrice"].ToString();
+                    date = date.Substring(0, date.Length - 11); //cuts off annoying timestamp 
+
+
+
+
+                    dataGridView1.Rows.Add(eventID, name, description, date, time, price, vipPrice); //adds to datagridview
+
+
+                }
+                myReader.Close();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+            conn3.Close();
+            Console.WriteLine("Done.");
+
         }
     }
 }
